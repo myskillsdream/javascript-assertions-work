45e125f61f4ae06d4196072afc2cf59e1fc6ebee
myskillsdream <myskillsdream@gmail.com>
2022-08-08 20:59:53 +0100

    make assert.isAbove and assert.isAtMost

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -62,7 +62,7 @@ suite('Unit Tests', function () {
     // #8
     test('#isAbove, #isAtMost', function () {
       assert.isAtMost('hello'.length, 5);
-      assert.isAtMost(1, 0);
+      assert.isAbove(1, 0);
       assert.isAbove(Math.PI, 3);
       assert.isAtMost(1 - Math.random(), 1);
     });
