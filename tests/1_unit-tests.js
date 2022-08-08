077e52971647a60fb2620958c1ae7afd425952c4
myskillsdream <myskillsdream@gmail.com>
2022-08-08 21:05:30 +0100

    make assert.isAbove and assert.isAtMost

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -68,7 +68,7 @@ suite('Unit Tests', function () {
     });
     // #9
     test('#isBelow, #isAtLeast', function () {
-      assert.isAtLeastl('world'.length, 5);
+      assert.isAtLeast('world'.length, 5);
       assert.isAtLeast(2 * Math.random(), 0);
       assert.isBelow(5 % 2, 2);
       assert.isBelow(2 / 3, 1);
