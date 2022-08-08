ea6bbfad8d7bc403f9bdd7f0a897ce7b57b71ec8
myskillsdream <myskillsdream@gmail.com>
2022-08-08 21:36:13 +0100

    make assert.isString and assert.isNotString

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -107,7 +107,7 @@ suite('Unit Tests', function () {
     test('#isString, #isNotString', function () {
       assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string');
       assert.isString(process.env.PATH, 'An env variable is a string (or undefined)');
-      assert.isNotString(JSON.stringify({ type: 'object' }), 'JSON is a string');
+      assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string');
     });
     // #14
     test('String #include, #notInclude', function () {
